4a665dc8fe9fdc44097585257f0c5156bc725d16
MaxSvargal <maxsvargal@gmail.com>
2016-12-24 17:30:44 +0300

    NAT transport for seed, add heroku launch seed script

--- seed.js
+++ seed.js
@@ -1,17 +1,22 @@
 const kad = require('kad')
+const traverse = require('kad-traverse')
 const zlib = require('zlib')
 
 const cb = () => {}
+const address = { address: '127.0.0.1', port: 1330 }
+const transportProps = {
+  traverse: {
+    upnp: { forward: 1330, ttl: 6000 },
+    stun: { server: { address: 'stun1.l.google.com', port: 19302 } },
+    turn: false
+  }
+}
 
-const dht = new kad.Node({
-  transport: kad.transports.UDP(
-    kad.contacts.AddressPortContact({
-      address: '127.0.0.1',
-      port: 1330
-    })
-  ),
-  storage: kad.storage.MemStore()
-})
+const contact = kad.contacts.AddressPortContact(address)
+const NatTransport = traverse.TransportDecorator(kad.transports.UDP)
+const transport = new NatTransport(contact, transportProps)
+const storage = kad.storage.MemStore()
+const dht = new kad.Node({ transport, storage })
 
 const topics = {
   '0c737f02449f8f669e694e3f11ff44c7f74b9e42': {
